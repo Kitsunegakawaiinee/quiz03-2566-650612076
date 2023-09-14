107dfe75a4e7044f3e2b7beb7244cbda8175c729
SKOT <skotskip@gmail.com>
2023-09-14 14:16:53 +0700

    user

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -1,20 +1,36 @@
 import jwt from "jsonwebtoken";
 
 import { DB, readDB } from "@/app/libs/DB";
-import { NextResponse } from "next/server";
+import { NextResponse, userAgent } from "next/server";
 
 export const POST = async (request) => {
   readDB();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  // my code
+  //
+  const body = await request.json();
+  const { username, password } = body;
 
-  const token = "Replace this with token creation";
+  readDB();
+  const check = DB.users.find(
+    (x) => x.username === username && x.password === password
+  );
+
+  if(!check)
+  return NextResponse.json(
+    {
+      ok: false,
+      message: "Username or Password is incorrect",
+    },
+    { status: 400 }
+  );
+
+  // const token = "Replace this with token creation";
+  const token = jwt.sign(
+    { username, role: check.role},
+    process.env.JWT_SECRET,
+    { expiresIn: "1h"}
+  );
 
   return NextResponse.json({ ok: true, token });
 };
